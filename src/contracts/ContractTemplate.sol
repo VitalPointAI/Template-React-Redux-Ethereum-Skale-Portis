d3b07874960d27e2c5e17d08f6678693409301d2
ALuhning <a.luhning@vitalpoint.ai>
2019-11-03 08:00:41 -0500

    added contract template

---
+++ src/contracts/ContractTemplate.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.5.0;
+
+// import statements
+
+// interfaces
+
+// libraries
+
+// Contracts
+contract nameOfContract {
+
+    // Type declarations
+
+    // State variables
+
+    // Events
+
+    //Functions
+    constructor() public {
+        //...
+    }
+
+    function() external {
+        //...
+    }
+
+    // External functions
+    // ...
+
+    // External functions that are view
+    // ...
+
+    // External functions that are pure
+    // ...
+
+    // Public functions
+    // ...
+
+    // Internal functions
+    // ...
+
+    // Private functions
+    // ...
+
+}
